b49abc2588bbd86c76f65c3c01f25da6e69b4995
Luhivskyi <luhivskyi.d@ukr.net>
2021-01-20 15:49:25 +0300

    Задача. Хранилище  6
    Напиши функцию-конструктор Storage, которая будет создавать объекты для управления складом товаров. Функция ожидает только один аргумент - начальный массив товаров, который записывается на создаваемый объект в свойство items.
    
    Добавь методы на прототип:
    
    getItems() - возвращает массив текущих товаров в свойстве items объекта, который вызывает этот метод.
    addItem(newItem) - принимает новый товар newItem и добавляет его в массив товаров в свойстве items объекта, который вызывает этот метод.
    removeItem(item) - принимает товар item и удаляет его из массива товаров в свойстве items объекта, который вызывает этот метод.
    Под комментарием мы добавили инициализацию экземпляра и вызовы методов в тойпоследовательности, в которой твой код будут проверять тесты. Пожалуйста ничего там не меняй.

---
+++ js/script6.js
@@ -0,0 +1,28 @@
+function Storage(items) {
+  this.items = items;
+}
+
+Storage.prototype.getItems = function () {
+  return this.items;
+};
+
+Storage.prototype.addItem = function (newItem) {
+  return this.items.push(newItem);
+  
+};
+
+Storage.prototype.removeItem = function () {
+  this.items.indexOf(this.items);
+  this.items.splice(1,1);
+};
+
+
+
+
+// Пиши код выше этой строки
+const storage = new Storage(['Нанитоиды', 'Пролонгер', 'Антигравитатор']);
+console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор"]
+storage.addItem('Дроид');
+console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
+storage.removeItem('Пролонгер');
+console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
